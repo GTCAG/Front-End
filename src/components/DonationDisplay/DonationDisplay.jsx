a24b0431516b61a10c77ffa01376ab33d4c31f4a
Mark Artishuk <markyshuk@gmail.com>
2019-12-17 15:51:06 -0800

    Replaced more fontawesome icons with material icons

--- src/components/DonationDisplay/DonationDisplay.jsx
+++ src/components/DonationDisplay/DonationDisplay.jsx
@@ -1,12 +1,22 @@
 import React from "react";
 import "./DonationDisplay.scss";
+import PaymentRoundedIcon from "@material-ui/icons/PaymentRounded";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles(() => ({
+  icon: {
+    fontSize: "40px",
+    marginRight: "20px"
+  }
+}));
 
 const DonationDisplay = () => {
+  const classes = useStyles();
   return (
     <div className="info-card shadow">
       <h2>Donate</h2>
       <div className="donation">
-        <i className="fas fa-wallet fa-2x"></i>
+        <PaymentRoundedIcon className={classes.icon} />
         <p>Card/Cash</p>
       </div>
       <div className="donation">
--- src/components/MediaCard/MediaCard.jsx
+++ src/components/MediaCard/MediaCard.jsx
@@ -1,25 +1,38 @@
 import React from "react";
 import "./MediaCard.scss";
+import YouTubeIcon from "@material-ui/icons/YouTube";
+import FacebookIcon from "@material-ui/icons/Facebook";
+import InstagramIcon from "@material-ui/icons/Instagram";
+import { makeStyles } from "@material-ui/core/styles";
 
+const useStyles = makeStyles(() => ({
+  icon: {
+    fontSize: "40px",
+    marginRight: "20px",
+    color: "#444"
+  }
+}));
 const MediaCard = () => {
+  const classes = useStyles();
   return (
     <div className="info-card shadow">
       <h2>Get Connected</h2>
       <div className="media">
         <a href="https://www.youtube.com/channel/UCNaKPci4jHkzFyo2hYwlVRQ">
-          <i className="fab fa-youtube fa-2x youtube"></i>
+          {/* <i className="fab fa-youtube fa-2x youtube"></i> */}
+          <YouTubeIcon className={classes.icon} />
           <p>Youtube</p>
         </a>
       </div>
       <div className="media">
         <a href="https://www.facebook.com/GraceTrinityChurch/?ref=nf&hc_ref=ART03pEjMPC3cd-tbTp_iAhT6IPlHWrCdI2wgyh_HWggVSb27aafVQL5ELahZNU9vZY">
-          <i className="fab fa-facebook-f fa-2x facebook"></i>
+          <FacebookIcon className={classes.icon} />
           <p>Facebook</p>
         </a>
       </div>
       <div className="media">
         <a href="">
-          <i className="fab fa-instagram fa-2x instagram"></i>
+          <InstagramIcon className={classes.icon} />
           <p>Instagram</p>
         </a>
       </div>
