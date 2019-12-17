553fed3dcf5a059d11219ce0620b7f761f757d92
Mark Artishuk <markyshuk@gmail.com>
2019-12-17 15:08:20 -0800

    Added animation on load for better user experience

--- src/components/Dashboard/DashboardCard.jsx
+++ src/components/Dashboard/DashboardCard.jsx
@@ -1,12 +1,26 @@
 import React from "react";
 
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
+
+const fadeInAndUp = keyframes`
+0% {
+  transform: translateY(15%);
+  opacity: 0.1;
+}
+
+100% {
+  transform: translateY(0);
+  opacity: 1;
+}
+`;
 
 const Card = styled.div`
   box-shadow: 0px 4px 15px 0px rgba(0, 0, 0, 0.555);
   background-color: #394359;
   height: 450px;
   color: #f2be8d;
+
+  animation: ${fadeInAndUp} 0.4s ease-out 0s;
 
   .what {
     padding: 10px 25px;
