ca40759ed0b4f5af8ece24a4178e12e0bd79f7da
Mark Artishuk <markyshuk@gmail.com>
2020-01-29 01:18:45 -0800

    Removed old Menu

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import DashboardMenu from "./DashboardMenu/DashboardMenu";
 import { Route } from "react-router-dom";
 import styled from "styled-components";
 import DashboardHome from "./DashboardHome";
