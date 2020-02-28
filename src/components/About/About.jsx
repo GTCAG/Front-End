9054af3a8ae26d7fad87c9cd32cb0076d77fcf0b
Mark Artishuk <markyshuk@gmail.com>
2020-02-27 22:26:09 -0800

    Added alexandr kalinyuk pastor bio for about section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./About.scss";
 import styled from "styled-components";
+import bioPic from "../../images/pastor_bio.png";
 
 const Container = styled.div`
   // background-color: red;
@@ -13,7 +14,7 @@ const Container = styled.div`
 const Bubble = styled.div`
   color: #333;
   background-color: white;
-  margin: 35px;
+  margin: 25px;
   width: 100%;
   max-width: 1500px;
   padding: 25px;
@@ -27,10 +28,103 @@ const Bubble = styled.div`
   }
 `;
 
+const FlexContainer = styled.div`
+  display: flex;
+
+  @media (max-width: 850px) {
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+`;
+
+const Image = styled.img`
+  width: 400px;
+  height: 652.7px;
+  border-radius: 5px;
+`;
+
+const SideText = styled.p`
+  font-family: "Lato", sans-serif;
+  font-size: 16px;
+  line-height: 1.85;
+  text-align: justify;
+  color: #555;
+`;
+
+const SideContainer = styled.div`
+  padding-right: 10px;
+  margin-left: 40px;
+`;
+
+const BioTitle = styled.h2`
+  font-size: 25px;
+  margin-bottom: 5px;
+`;
+
+const BioSubTitle = styled.p`
+  font-family: "Lato", sans-serif;
+
+  margin: 0px;
+  padding: 0px;
+
+  color: #888;
+`;
+
 const About = () => {
   return (
     <Container>
-      <Bubble>Test</Bubble>
+      <Bubble>
+        <FlexContainer>
+          <Image src={bioPic} alt="Pastor" />
+          <SideContainer>
+            <BioTitle>Aleksandr Kalinyuk</BioTitle>
+            <BioSubTitle>
+              Bishop, Regional Pastor of the National Slavic District of
+              Assembly of God, Southwestern Region, United States
+            </BioSubTitle>
+            <SideText>
+              Aleksandr Kalinyuk was born in a Christian family and his father
+              was a pastor in the city Novovolynsk, Ukraine. From early
+              childhood his life was spent in church. It is for this reason, at
+              the age of eight, he was “born again” – God gave him the Holy
+              Spirit. When he was 17 years old, Aleksandr was baptized in water,
+              which symbolized the death, burial and resurrection of Jesus
+              Christ and his identification with Him. Right after this,
+              Aleksandr actively involved himself in serving the Lord in his
+              local church. He worked with youth groups and directed the gospel
+              musical group.
+            </SideText>
+
+            <SideText>
+              In 1989 he permanently immigrated to the United States. In 1991 he
+              was ordained for a deacon’s service, in 1995 ordained to be a
+              pastor, and in 1996 became the senior pastor of the “Grace Trinity
+              Church” located in Sacramento, California where he presently
+              serves God.
+            </SideText>
+
+            <SideText>
+              In 1998 Aleksandr was elected as a senior pastor of the California
+              United Slavic evangelical churches, west. There he actively
+              participated in constructing the new National Slavic District and
+              in 2013 he was conference elected as a Regional pastor of the
+              National Slavic District of Assemblies of God in the Southwest
+              region.
+            </SideText>
+
+            <SideText>
+              Aleksandr Kalinyuk graduated from the Evangelical college in
+              Chicago and biblical institute in Fresno, California.
+            </SideText>
+
+            <SideText>
+              He has been married to his wife, Svetlana, for 35 years. They have
+              five children and 13 grandchildren – all boys!
+            </SideText>
+          </SideContainer>
+        </FlexContainer>
+      </Bubble>
     </Container>
   );
 };
