cdf6d6750dfe81daa6670ab11330ea3f62c054c8
Mark Artishuk <markyshuk@gmail.com>
2020-03-04 00:34:50 -0800

    Added more visuals to song list in event view

---
+++ src/components/Dashboard/EventView/EventSongList.jsx
@@ -0,0 +1,91 @@
+import React from "react";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
+import MusicNoteIcon from "@material-ui/icons/MusicNote";
+import ListItemText from "@material-ui/core/ListItemText";
+import NavigateNextIcon from "@material-ui/icons/NavigateNext";
+import Card from "@material-ui/core/Card";
+import Tooltip from "@material-ui/core/Tooltip";
+
+import DeleteIcon from "@material-ui/icons/Delete";
+import IconButton from "@material-ui/core/IconButton";
+
+import Typography from "@material-ui/core/Typography";
+import AppBar from "@material-ui/core/AppBar";
+
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    boxSizing: "border-box",
+    width: "80%"
+  },
+  songTitle: {
+    color: "#555"
+  },
+  card: {
+    marginBottom: 20,
+    width: "100%",
+    boxSizing: "border-box"
+  },
+  barRoot: {
+    padding: 5,
+    backgroundColor: "#37474f"
+  },
+  title: {
+    textAlign: "center"
+  },
+  nextIcon: {
+    marginLeft: 10
+  }
+}));
+
+const EventSongList = ({ songs, admin }) => {
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <Card className={classes.card}>
+        <AppBar className={classes.barRoot} position="static">
+          <Typography variant="h6" className={classes.title}>
+            Songs
+          </Typography>
+        </AppBar>
+        <List className={classes.list}>
+          <ListItem>
+            <ListItemIcon>
+              <MusicNoteIcon />
+            </ListItemIcon>
+            <ListItemText
+              className={classes.songTitle}
+              primary="Example Song"
+            />
+
+            <ListItemSecondaryAction>
+              {admin ? (
+                <Tooltip title="Remove">
+                  <IconButton edge="end" aria-label="delete">
+                    <DeleteIcon />
+                  </IconButton>
+                </Tooltip>
+              ) : null}
+
+              <Tooltip title="View">
+                <IconButton
+                  className={classes.nextIcon}
+                  edge="end"
+                  aria-label="next"
+                >
+                  <NavigateNextIcon />
+                </IconButton>
+              </Tooltip>
+            </ListItemSecondaryAction>
+          </ListItem>
+        </List>
+      </Card>
+    </div>
+  );
+};
+
+export default EventSongList;
--- src/components/Dashboard/EventView/EventView.jsx
+++ src/components/Dashboard/EventView/EventView.jsx
@@ -2,8 +2,10 @@ import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { makeStyles } from "@material-ui/core";
 import Typography from "@material-ui/core/Typography";
+import dateFormat from "dateformat";
 
 import { axiosAuth } from "../../../axiosWithAuth";
+import EventSongList from "./EventSongList";
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -17,28 +19,42 @@ const useStyles = makeStyles(theme => ({
   },
   titleContainer: {
     display: "flex",
+    flexDirection: "column",
     justifyContent: "space-between",
     alignItems: "center"
   },
   eventName: {
     margin: 0,
-    padding: 0
+    padding: 0,
+    fontSize: "35px"
   },
   eventDate: {
-    color: "#898989",
-    paddingLeft: 30,
-    borderLeft: "1px solid gray"
+    color: "#898989"
   },
   cardContainer: {
     background: "#FFFFFF",
     boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
     borderRadius: 15,
     padding: 20
+  },
+  headerTitle: {
+    margin: 0,
+    padding: 0,
+    marginTop: 25
+  },
+  songsContainer: {
+    display: "flex",
+    alignItems: "center",
+    flexDirection: "column",
+    marginTop: 40
   }
 }));
 
 const EventView = () => {
-  const [event, setEvent] = useState({});
+  const [event, setEvent] = useState({
+    date: Date.now(),
+    name: ""
+  });
   const classes = useStyles();
   const { eventId } = useParams();
 
@@ -48,6 +64,7 @@ const EventView = () => {
       .get(`/events/${eventId}`)
       .then(res => {
         console.log("Event view res:", res);
+        setEvent(res.data);
       })
       .catch(err => {
         console.log("Err: ", err);
@@ -57,14 +74,15 @@ const EventView = () => {
     <div className={classes.root}>
       <div className={classes.cardContainer}>
         <div className={classes.titleContainer}>
-          <h2 className={classes.eventName}>Event Title</h2>
+          <h2 className={classes.eventName}>{event.name}</h2>
           <Typography variant="h6" className={classes.eventDate}>
-            {/* {dateFormat(localDate, "mmmm dS, h:MM TT")} */}
-            Event Date
+            {dateFormat(event.date, "mmmm dS, h:MM TT")}
           </Typography>
         </div>
+        <div className={classes.songsContainer}>
+          <EventSongList admin={false} />
+        </div>
       </div>
-      <div></div>
     </div>
   );
 };
